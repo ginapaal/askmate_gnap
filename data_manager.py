f0b2ccf92c16d935792faa1bb778d84876f65cd7
davidtanacs <tanacsdavid90@gmail.com>
2017-05-09 18:41:16 +0200

    In data_manager.py read_from_csv done

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,6 @@
+import base64
+
+
 def ID_generator(table):
     new_id = [0]
     for row in table:
@@ -6,11 +9,14 @@ def ID_generator(table):
     return str(new_id)
 
 
-
-#def read_from_csv():
-    #return table
-
-
+def read_from_csv(filename):
+    table = []
+    with open(filename, "r") as data:
+        for line in data:
+            lines = line.replace("\n", "")
+            words = lines.split(',')
+            table.append(words)
+    return table
 
 
 #def write_to_csv():
