74490f386160b1d1880b0789f9f3b5b7906f2f7d
Gina <paalginaa@gmail.com>
2017-05-10 17:35:09 +0200

    reversed list done, table done, answer buttons done

--- answer.py
+++ answer.py
@@ -3,12 +3,13 @@ from data_manager import *
 
 app = Flask(__name__)
 
+
 @app.route("/new_asnwer", methods=["GET", "POST"])
 def new_answer():
     if request.method == "POST":
         table = read_from_csv('answers.csv')
-        answer_list=[]
-        answer_list.append(ID_generator(table) (request.form('new_answer'))
+        answer_list = []
+        answer_list.append(ID_generator(table)(request.form('new_answer'))
         table.append(answer_list)
         write_to_csv('answers.csv')
         return render_template("answers.html")
