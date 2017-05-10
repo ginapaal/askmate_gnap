de872287cb8e742031b6e9fbbd01630d03d283dd
Viktor Matrai <viktor0matrai@gmail.com>
2017-05-10 14:46:10 +0200

    answer module, and html templatestrying to build the answer text box and answer list onto one template

---
+++ answer.py
@@ -0,0 +1,18 @@
+from flask import *
+from data_manager import *
+
+app = Flask(__name__)
+
+@app.route("/new_asnwer", methods=["GET", "POST"])
+def new_answer():
+    if request.method == "POST":
+        table = read_from_csv('answers.csv')
+        answer_list=[]
+        answer_list.append(ID_generator(table) (request.form('new_answer'))
+        table.append(answer_list)
+        write_to_csv('answers.csv')
+        return render_template("answers.html")
+    else return render_template("answes.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
