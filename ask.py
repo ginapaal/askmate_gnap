a28879f33b733798393194d2e4a4d3f2aff364ad
orpolip <galdonyi0109@gmail.com>
2017-05-10 13:32:44 +0200

    added the ask function, tested

---
+++ ask.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from data_manager import *
+import csv
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET"])
+@app.route('/list', methods=["GET"])
+def display_list():
+    table = read_from_csv("question.csv")
+    return render_template("list.html", table=table)
+
+
+@app.route("/new_question", methods=['GET', 'POST'])
+def new_question():
+    if request.method == 'POST':
+        table = read_from_csv("question.csv")
+        question_list = []
+        question_list.append(ID_generator(table))
+        question_list.append(request.form['new_question'])
+        table.append(question_list)
+        write_to_csv("question.csv", table)
+
+        return render_template("list.html", table=table)
+    else:
+        return render_template("ask_a_question.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- data_manager.py
+++ data_manager.py
@@ -5,7 +5,8 @@ def ID_generator(table):
     new_id = [0]
     for row in table:
         new_id.append(int(row[0]))
-        new_id = max(new_id) + 1
+    new_id = max(new_id) + 1
+
     return str(new_id)
 
 
@@ -20,12 +21,7 @@ def read_from_csv(filename):
 
 
 def write_to_csv(filename, new_data):
-    table = read_from_csv(filename)
-    new_id = ID_generator(table)
-    new_data.insert(0, new_data)
     with open(filename, "a") as table:
-        for data in table:
-            f.write(data + ",")
-        f.write('\n')
-    updated_table = read_from_csv(filename)
-    return updated_table
+        for word in new_data:
+            table.write(str(word) + ",")
+        table.write('\n')
