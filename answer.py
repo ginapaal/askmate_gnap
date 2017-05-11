051f6d2bbf6b0cb5acb8c45106d93d81a2d9c4b2
Viktor Matrai <viktor0matrai@gmail.com>
2017-05-11 09:11:31 +0200

    answer list rendering

--- answer.py
+++ answer.py
@@ -3,7 +3,23 @@ from data_manager import *
 
 app = Flask(__name__)
 
-@app.route("/new_asnwer", methods=["GET", "POST"])
+@app.route('/', method= 'GET')
+@app.route('/list', method= 'GET')
+def direct_to_question():
+
+    with read_from_csv('list')
+
+@app.route('/', method= 'GET', 'POST')
+@app.route('/list', method='GET','POST')
+def display_answer_list()
+
+    table = read_from_csv('answers.csv')
+    reverse_answers_timeline = reversed(table)
+    return render_table('answers.html', table = reverse_answers_timeline )
+
+
+
+@app.route("/new_asnwer", method=["GET", "POST"])
 def new_answer():
     if request.method == "POST":
         table = read_from_csv('answers.csv')
