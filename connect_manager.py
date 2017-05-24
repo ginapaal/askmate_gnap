c190570f630a7a301f997b396ea7f72f4e45eb3a
Viktor Matrai <viktor0matrai@gmail.com>
2017-05-24 13:23:39 +0200

    writes the question, which you sould answer. Solved in main.py too, <h> styles added to writingpages.css

--- connect_manager.py
+++ connect_manager.py
@@ -2,6 +2,6 @@ import psycopg2
 
 
 def connect_to_db():
-    conn = psycopg2.connect("dbname='tanacs' user='tanacs' host='localhost' password='buggyanás'")
+    conn = psycopg2.connect("dbname='matraiv' user='matraiv' host='localhost' password='1989matraiv17'")
     conn.autocommit = True
     return conn
