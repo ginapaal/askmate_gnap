f7de1a5ca4842348a1167e5ab64a759cc43b5bf2
davidtanacs <tanacsdavid90@gmail.com>
2017-05-25 14:56:59 +0200

    Display_comment_list made

--- main.py
+++ main.py
@@ -41,21 +41,22 @@ def new_answer(question_id):
     if request.method == "POST":
 
         query_manager.insert_into_answer(connect_to_db(), question_id)
-        return redirect("/")
+        return redirect("/question/" + question_id + "")
     else:
         return render_template('write_new_answer.html', question_id=question_id, question=question,
                                question_body=question_body)
 
 
-@app.route("/question/<question_id>/comments", methods=['GET'])
+@app.route("/question/<question_id>/comments", methods=['POST', 'GET'])
 def display_comment_list(question_id):
-    rows_comments = query_manager.read_from_q_comments(connect_to_db, (question_id))
-    rows_question = query_manager.read_from_question(connect_to_db(), question_id)
+    rows_comments = query_manager.show_db_item(connect_to_db(), """SELECT message FROM comment WHERE question_id=%s;""", question_id)
+    question = query_manager.show_db_item(connect_to_db(), """SELECT title FROM question WHERE id=%s;""", question_id)
+    question_body = query_manager.show_db_item(connect_to_db(), """SELECT message FROM question WHERE id=%s;""", question_id)
     if rows_comments == []:
         rows_comments = ""
-    return render_template("question_comment.html", comment=rows_comments, question_id=question_id, question=rows_question[0][4], question_body=rows_question[0][5])
 
-
+    return render_template("question_comment.html", comment=rows_comments, question_id=question_id, question=question,
+                           question_body=question_body)
 
 
 if __name__ == "__main__":
